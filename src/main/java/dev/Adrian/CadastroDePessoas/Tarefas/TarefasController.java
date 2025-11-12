973b50bdc0d1cb478e77e53002ffde70522dd357
AdrianLorenzo-br <adrian.lorenzo.c.faria@gmail.com>
2025-11-12 17:17:09 -0300

    refactor: endpoints implementados na classe controller de tarefas, integrando com o service

--- src/main/java/dev/Adrian/CadastroDePessoas/Tarefas/TarefasController.java
+++ src/main/java/dev/Adrian/CadastroDePessoas/Tarefas/TarefasController.java
@@ -1,37 +1,67 @@
 package dev.Adrian.CadastroDePessoas.Tarefas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("tarefas")
 public class TarefasController {
 
+    public final TarefasService tarefasService;
+
+    public TarefasController(TarefasService tarefasService) {
+        this.tarefasService = tarefasService;
+    }
+
     @PostMapping("/criar")
-    public ResponseEntity<TarefasDTO> criarTarefa(TarefasDTO TarefasRequest) {
-        return null;
+    public ResponseEntity<String> criarTarefa(@RequestBody TarefasDTO TarefasRequest) {
+        TarefasDTO novaTarefa = tarefasService.criarTarefa(TarefasRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Tarefa Criada com Sucesso: " + novaTarefa.getNome() + " ID: " + novaTarefa.getId());
     }
 
     @GetMapping("/listar")
-    public String listarTodasAsTarefas() {
-        return "Mostrar Tarefa";
+    public ResponseEntity<List<TarefasDTO>> listarTarefas() {
+        List<TarefasDTO> tarefas = tarefasService.listarTarefas();
+        return ResponseEntity.ok(tarefas);
+    }
+
+    @GetMapping("/listar/${id}")
+    public ResponseEntity<?> listarTarefaPorID(@PathVariable Long id, @RequestBody TarefasDTO TarefasRequest) {
+        TarefasDTO tarefaExistente = tarefasService.listarTarefasPorId(id);
+        if (tarefaExistente != null) {
+            return ResponseEntity.ok(tarefaExistente);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Tarefa de ID: " + id + " não existe");
+
     }
 
 
-    @GetMapping("/listarID")
-    public String listarTarefaPorID() {
-        return "Mostrar Tarefa por id";
-    }
-
-
-    @PutMapping("/alterarID")
-    public String alterarTarefaPorId() {
-        return "Alterar Tarefa por id";
+    @PutMapping("/alterar/${id}")
+    public ResponseEntity<?> alterarTarefaPorId(@PathVariable Long id, @RequestBody TarefasDTO TarefasRequest) {
+        TarefasDTO tarefaExistente = tarefasService.listarTarefasPorId(id);
+        if (tarefaExistente != null) {
+            TarefasDTO tarefaSalva = tarefasService.alterarTarefasPorId(id, TarefasRequest);
+            return ResponseEntity.ok(tarefaSalva);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Tarefa de ID: " + id + " não existe");
     }
 
 
     @DeleteMapping("/deletarID")
-    public String deletarTarefaPorId() {
-        return "Tarefa deletada por id";
+    public ResponseEntity<String> deletarTarefaPorId(Long id) {
+        TarefasDTO tarefaExistente = tarefasService.listarTarefasPorId(id);
+        if (tarefaExistente != null) {
+            tarefasService.DeletarTarefasPorId(id);
+            return ResponseEntity.ok("Tarefa Deletado com Sucesso");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa de ID: " + id + " não existe");
     }
 }
